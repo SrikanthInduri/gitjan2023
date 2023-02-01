73192f3ce05ca09b2f2da1c58fad780ad0dd8076
SrikanthInduri <indurisrikanthbabu@gmail.com>
2023-02-01 23:00:48 +0530

    first commit on my frame work

---
+++ src/com/srikanth/automation/gitpractice/GitDemo.java
@@ -0,0 +1,10 @@
+package com.srikanth.automation.gitpractice;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		System.out.println("hello boys do poractice");
+	}
+	
+	
+}
---
+++ src/com/srikanth/automation/gitpractice/GitDemoOne.java
@@ -0,0 +1,8 @@
+package com.srikanth.automation.gitpractice;
+
+public class GitDemoOne {
+
+	public static void main(String[] args) {
+		System.out.println("hardwork gives job");
+	}
+}
