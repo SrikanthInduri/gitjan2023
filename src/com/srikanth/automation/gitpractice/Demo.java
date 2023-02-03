769042fda750b020542e74130b1db1e27fde8c9f
SrikanthInduri <indurisrikanthbabu@gmail.com>
2023-02-03 19:56:15 +0530

    add data Demo.java by archit

--- src/com/srikanth/automation/gitpractice/Demo.java
+++ src/com/srikanth/automation/gitpractice/Demo.java
@@ -4,6 +4,8 @@ public class Demo {
  public static void main(String[] args) {
 	System.out.println("job is mandatery");
 	System.out.println("java");
+System.out.println("java srikanth  ");
+System.out.println("hello");
 }
 	
 	
